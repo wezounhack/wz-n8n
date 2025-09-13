3144a7da7a0bac59949f4e4343c71992c1e18991
wezounhack <150855340+wezounhack@users.noreply.github.com>
2025-09-13 18:27:03 +0100

    Update deploy_n8n.sh
    initial config

--- deploy_n8n.sh
+++ deploy_n8n.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+docker pull wezoun/n8n:latest
+
+# Arrête et supprime l'ancienne instance
+docker stop n8n || true
+docker rm n8n || true
+
+# Démarre la nouvelle instance
+docker run -d --name n8n -p 5678:5678 wezoun/n8n:latest
 
